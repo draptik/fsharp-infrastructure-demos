dcf7553903238e9dd585e2b07bd7dac31fd55c4b
Patrick Drechsler <patrick@pdrechsler.de>
2021-01-18 19:15:30 +0100

    web aspnetcore webapi: trying dapper w/ sqlite - initial code from docs

--- fsharp-web/demo2-aspnetcore-webapi/WebApplication/WebApplication/Startup.fs
+++ fsharp-web/demo2-aspnetcore-webapi/WebApplication/WebApplication/Startup.fs
@@ -6,12 +6,41 @@ open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 
+// https://github.com/simonfaltum/AspNetCore.Identity.Dapper
+// nuget package names:
+// - VeryGood.AspNetCore.Identity
+// - VeryGood.AspNetCore.Identity.DatabaseScripts.DbUp
+open AspNetCore.Identity.Dapper
+open AspNetCore.Identity.Dapper.Models
+open AspNetCore.Identity.DatabaseScripts.DbUp
+
 type Startup(configuration: IConfiguration) =
     member _.Configuration = configuration
 
     // This method gets called by the runtime. Use this method to add services to the container.
     member _.ConfigureServices(services: IServiceCollection) =
         // Add framework services.
+        
+        let myDbFilename = "app.sqlite"
+        let myConnectionString = sprintf "Data Source=%s;Version=3;" myDbFilename
+        let myDbSchema = "foo"
+        
+        services
+            .AddIdentityCore<ApplicationUser>(fun options ->
+                options.User.AllowedUserNameCharacters <- "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+"
+                options.User.RequireUniqueEmail <- true
+                options.Password.RequireDigit <- false
+                )
+            .AddRoles<ApplicationRole>()
+            .AddDapperStores(fun options ->
+                options.ConnectionString <- myConnectionString
+                options.DbSchema <- myDbSchema) |> ignore
+        
+        services
+            .AddIdentityDbUpDatabaseScripts(fun options ->
+                options.ConnectionString <- myConnectionString
+                options.DbSchema <- myDbSchema) |> ignore
+        
         services.AddControllers() |> ignore
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
